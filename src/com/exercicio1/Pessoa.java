c406d08599e0081a89f48560b5be02733319340b
Rodrigo Silva <rdrsantos014@gmail.com>
2021-10-27 14:48:22 -0300

    Initial commit

---
+++ src/com/exercicio1/Pessoa.java
@@ -0,0 +1,31 @@
+package com.exercicio1;
+
+public class Pessoa {
+    private String nome;
+    private String dataNascimento;
+    private double altura;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+}
