204ab8a02aba5074d040a226cbd545730db3f68b
Rodrigo Silva <rdrsantos014@gmail.com>
2021-10-27 15:46:14 -0300

    exercicio 1

--- src/com/exercicio1/Main.java
+++ src/com/exercicio1/Main.java
@@ -3,6 +3,11 @@ package com.exercicio1;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Pessoa rodrigo = new Pessoa("Rodrigo", "21/02/2000", 1.7); // data com separada por /
+        Pessoa maria = new Pessoa("Maria", "12-07-1998", 1.6); // data separada por -
+
+        System.out.println(rodrigo.apresentar());
+        System.out.println();
+        System.out.println(maria.apresentar());
     }
 }
--- src/com/exercicio1/Pessoa.java
+++ src/com/exercicio1/Pessoa.java
@@ -1,12 +1,41 @@
 package com.exercicio1;
 
+import java.util.Calendar;
+
 public class Pessoa {
     private String nome;
     private String dataNascimento;
     private double altura;
 
+    public Pessoa(String nome, String dataNascimento, double altura){
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.altura = altura;
+    }
+
+    public String apresentar(){
+        return "Nome: " + this.getNome() + "\n" +
+                "Data de Nascimento: " + this.getDataNascimento() + "\n" +
+                "Altura: " + this.getAltura() + "\n" +
+                "Idade: " + getIdade();
+    }
+
+    public int getIdade(){
+        String dataNascimentoArray[] = this.dataNascimento.split("/");
+        int dataComTraco = this.dataNascimento.indexOf("-");
+
+        if (dataComTraco >= 0){ //verifica se a data veio no padrao ""00-00-0000"
+            dataNascimentoArray = this.dataNascimento.split("-");
+        }
+
+        int anoNascimento = Integer.parseInt(dataNascimentoArray[dataNascimentoArray.length-1]);
+        Calendar data = Calendar.getInstance();
+        int anoAtual = data.get(Calendar.YEAR);
+        return anoAtual - anoNascimento;
+    }
+
     public String getNome() {
-        return nome;
+        return this.nome;
     }
 
     public void setNome(String nome) {
@@ -14,7 +43,7 @@ public class Pessoa {
     }
 
     public String getDataNascimento() {
-        return dataNascimento;
+        return this.dataNascimento;
     }
 
     public void setDataNascimento(String dataNascimento) {
@@ -22,7 +51,7 @@ public class Pessoa {
     }
 
     public double getAltura() {
-        return altura;
+        return this.altura;
     }
 
     public void setAltura(double altura) {
